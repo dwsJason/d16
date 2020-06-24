47d87dd4905de273d4aa8a7566e7450e8f817468
JASON-6700K\jandersen <JGAndersen@gmail.com>
2020-06-24 13:34:16 -0400

    toolbar: small code cleanup

--- source/toolbar.cpp
+++ source/toolbar.cpp
@@ -9,34 +9,6 @@
 #include "sdl_helpers.h"
 
 
-#if 0
-#include "imgui.h"
-#include "imgui_impl_sdl.h"
-#include "imgui_impl_opengl3.h"
-#include <stdio.h>
-#include <SDL.h>
-
-
-// This bit here is also dumb
-// About Desktop OpenGL function loaders:
-//  Modern desktop OpenGL doesn't have a standard portable header file to load OpenGL function pointers.
-//  Helper libraries are often used for this purpose! Here we are supporting a few common ones (gl3w, glew, glad).
-//  You may use another loader/header of your choice (glext, glLoadGen, etc.), or chose to manually implement your own.
-#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
-#include <GL/gl3w.h>    // Initialize with gl3wInit()
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
-#include <GL/glew.h>    // Initialize with glewInit()
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
-#include <glad/glad.h>  // Initialize with gladLoadGL()
-#else
-#include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
-#endif
-
-// Again, this should be in some sort of helper, or util header
-extern GLuint
-SDL_GL_LoadTexture(SDL_Surface * surface, GLfloat * texcoord);
-#endif
-
 //------------------------------------------------------------------------------
 Toolbar* Toolbar::GToolbar = nullptr;
 //------------------------------------------------------------------------------
