2e971bcf4af6ba9d40996c79ad3bcfad0a1d3006
JASON-6700K\jandersen <JGAndersen@gmail.com>
2020-09-01 10:48:23 -0400

    import: gsdx_file module

---
+++ source/gsdx_file.cpp
@@ -0,0 +1,22 @@
+//
+// C++ Encoder
+// For GSdx, a tool for pre-compiling scroll code on the IIgs
+// 
+// File Format summary here from Brutal Deluxe
+//
+//
+#include "gsdx_file.h"
+#include <stdio.h>
+
+//------------------------------------------------------------------------------
+
+GSDXFile::GSDXFile()
+{
+}
+
+GSDXFile::~GSDXFile()
+{
+}
+
+//------------------------------------------------------------------------------
+
---
+++ source/gsdx_file.h
@@ -0,0 +1,27 @@
+//
+// C++ Encoder
+// For GSdx, a tool for pre-compiling scroll code on the IIgs
+// 
+// File Format summary here from Brutal Deluxe
+//
+//
+
+#ifndef GSDX_FILE_H
+#define GSDX_FILE_H
+
+#include <vector>
+
+class GSDXFile
+{
+public:
+	GSDXFile();
+
+	~GSDXFile();
+
+private:
+
+};
+
+
+#endif // GSDX_FILE_H
+
